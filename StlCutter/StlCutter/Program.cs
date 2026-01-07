6416edefcc8e540ea9fc06ec3f52abe9fac704bd
Kilowatt6464 <alpha.maurycy@gmail.com>
2026-01-07 13:09:48 +0100

    Add files via upload

---
+++ StlCutter/StlCutter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StlCutter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int result = StlCutter.CutStl(
+                "model.stl",
+                "partA.stl",
+                "partB.stl",
+                0, 1, 0, 0); // płaszczyzna Y=0
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+
+}
---
+++ StlCutter/StlCutter/StlCutter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StlCutter
+{
+    public static class StlCutter
+    {
+        [DllImport("StlCutterLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CutStl(
+            string inputPath,
+            string outputA,
+            string outputB,
+            double planeX,
+            double planeY,
+            double planeZ,
+            double planeD);
+    }
+}
